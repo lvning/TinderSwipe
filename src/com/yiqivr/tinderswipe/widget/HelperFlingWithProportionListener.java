e0fcfd789a3007b120897b86463ced5ca3ecf59a
lvning <lvning126@126.com>
2014-09-25 10:54:32 +0800

    HelperFlingListener is no use now.delete it.

--- src/com/yiqivr/tinderswipe/widget/HelperFlingWithProportionListener.java
+++ src/com/yiqivr/tinderswipe/widget/HelperFlingWithProportionListener.java
@@ -2,7 +2,17 @@ package com.yiqivr.tinderswipe.widget;
 
 import android.view.View;
 
-public interface HelperFlingWithProportionListener extends HelperFlingListener {
+public interface HelperFlingWithProportionListener {
+	public void onCardExited();
+
+	public void leftExit(Object dataObject);
+
+	public void rightExit(Object dataObject);
+
+	public void topExit(Object dataObject);
+
+	public void bottomExit(Object dataObject);
+
 	public void flingResetOrigin(View selfView);
 
 	public void flingOccurPercent(View selfView, int percent, boolean swipeTop);
