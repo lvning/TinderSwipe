5558bd34468c080986962ca1f9100b0696c28bd9
lvning <lvning126@126.com>
2014-10-06 21:23:42 +0800

    Modify README file.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+TinderSwipe
+===========
+tinder-like effect
+
 ![image](https://github.com/lvning/TinderSwipe/blob/master/tinder-swipe.gif)
--- src/com/yiqivr/tinderswipe/MyActivity.java
+++ src/com/yiqivr/tinderswipe/MyActivity.java
@@ -4,20 +4,26 @@ import java.util.ArrayList;
 
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.view.PagerAdapter;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
 import android.view.animation.OvershootInterpolator;
+import android.view.animation.TranslateAnimation;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.yiqivr.tinderswipe.widget.CircleProgress;
 import com.yiqivr.tinderswipe.widget.FlingCardListener.SWIPEMODE;
+import com.yiqivr.tinderswipe.widget.MultiViewPager;
 import com.yiqivr.tinderswipe.widget.OnLeftRightFlingListener;
 import com.yiqivr.tinderswipe.widget.OnTopBottomFlingWithProportionListener;
 import com.yiqivr.tinderswipe.widget.SwipeFlingAdapterView;
@@ -25,10 +31,16 @@ import com.yiqivr.tinderswipe.widget.SwipeFlingAdapterView;
 public class MyActivity extends Activity implements OnLeftRightFlingListener, OnTopBottomFlingWithProportionListener {
 
 	private ArrayList<String> al;
+	private ArrayList<View> pagerView;
 	private MyAdapter adapter;
+	private MyPagerAdapter pagerAdapter;
 	private TextView boxPic;
 
 	private boolean addMoreExcuting;
+
+	private MultiViewPager pager;
+	private View out1, out2, shadow;
+	private final int duration = 500;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +49,19 @@ public class MyActivity extends Activity implements OnLeftRightFlingListener, On
 
 		SwipeFlingAdapterView flingContainer = (SwipeFlingAdapterView) findViewById(R.id.frame);
 		boxPic = (TextView) findViewById(R.id.boxpic);
+		pager = (MultiViewPager) findViewById(R.id.viewpager);
+		out1 = findViewById(R.id.boxpic_out1);
+		out2 = findViewById(R.id.boxpic_out2);
+		shadow = findViewById(R.id.shadow);
 
 		al = new ArrayList<String>();
+		pagerView = new ArrayList<View>();
 		for (int i = 1; i < 10; i++) {
 			al.add(i + "");
+			View v = LayoutInflater.from(MyActivity.this).inflate(R.layout.item, null);
+			v.findViewById(R.id.bottom_progress).setVisibility(View.GONE);
+			v.findViewById(R.id.top_progress).setVisibility(View.GONE);
+			pagerView.add(v);
 		}
 
 		flingContainer.setSwipeMode(SWIPEMODE.UP_DOWN);
@@ -49,6 +70,76 @@ public class MyActivity extends Activity implements OnLeftRightFlingListener, On
 //		flingContainer.setLeftRightFlingListener(this);
 		adapter = new MyAdapter();
 		flingContainer.setAdapter(adapter);
+
+		pager.setVisibility(View.GONE);
+		out1.setAlpha(0);
+		out2.setAlpha(0);
+
+		findViewById(R.id.collect_box).setOnClickListener(new OnClickListener() {
+
+			@SuppressLint("NewApi")
+			@Override
+			public void onClick(View v) {
+
+				pager.setAlpha(0);
+				shadow.setAlpha(0);
+				shadow.setVisibility(View.VISIBLE);
+				pager.setVisibility(View.VISIBLE);
+				pagerAdapter = new MyPagerAdapter();
+				pager.setPageMargin(-300);
+				pager.setAdapter(pagerAdapter);
+				pager.post(new Runnable() {
+
+					@Override
+					public void run() {
+						out1.setAlpha(1);
+						out2.setAlpha(1);
+						out1.clearAnimation();
+						out2.clearAnimation();
+						int transByY = pager.getTop() - out1.getTop();
+						final int bransByX = pager.getChildAt(1).getLeft() - pager.getChildAt(0).getLeft();
+						out1.animate().translationY(transByY).setDuration(duration);
+						shadow.animate().alpha(1).setDuration(2 * duration);
+						out2.animate().translationY(transByY).setDuration(duration).withEndAction(new Runnable() {
+
+							@Override
+							public void run() {
+								out2.animate().translationX(bransByX).setDuration(duration)
+										.withEndAction(new Runnable() {
+
+											@Override
+											public void run() {
+												pager.setAlpha(1);
+												shadow.setAlpha(1);
+												out1.setAlpha(0);
+												out2.setAlpha(0);
+											}
+										});
+							}
+						});
+					}
+				});
+			}
+		});
+
+		shadow.setOnClickListener(new OnClickListener() {
+
+			@SuppressLint("NewApi")
+			@Override
+			public void onClick(View v) {
+				Log.v("", "shadow clicked!!!");
+				shadow.animate().alpha(0).setDuration(2 * duration).withEndAction(new Runnable() {
+
+					@Override
+					public void run() {
+						shadow.setVisibility(View.GONE);
+					}
+				});
+				Log.e("", "pager.getChildCount() = " + pager.getChildCount());
+				
+			}
+		});
+		
 	}
 
 	@Override
@@ -154,6 +245,48 @@ public class MyActivity extends Activity implements OnLeftRightFlingListener, On
 			return v;
 		}
 
+	}
+
+	private class MyPagerAdapter extends PagerAdapter {
+
+		@Override
+		public int getCount() {
+			return pagerView.size();
+		}
+
+		@Override
+		public boolean isViewFromObject(View arg0, Object arg1) {
+			return arg0 == arg1;
+		}
+
+		@Override
+		public void destroyItem(ViewGroup container, int position, Object object) {
+			container.removeView(pagerView.get(position));
+		}
+
+		@Override
+		public Object instantiateItem(ViewGroup container, int position) {
+			container.addView(pagerView.get(position));
+			return pagerView.get(position);
+		}
+
+	}
+
+	private Animation getAnim(boolean isFirst) {
+		int duration = 2000;
+		Animation animation;
+		if (isFirst) {
+			animation = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF, 0.0f,
+					Animation.RELATIVE_TO_SELF, 1.0f, Animation.RELATIVE_TO_SELF, 0.0f);
+			animation.setDuration(duration);
+		} else {
+			animation = new TranslateAnimation(Animation.RELATIVE_TO_SELF, -1.0f, Animation.RELATIVE_TO_SELF, 0.0f,
+					Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF, 0.0f);
+			animation.setStartOffset(duration);
+			animation.setDuration(duration);
+		}
+
+		return animation;
 	}
 
 	@Override
